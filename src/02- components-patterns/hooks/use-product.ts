e4a326e089e17675a3ee6fa8d513afa831d9c5b2
Lula <lucianachamorro87@gmail.com>
2023-11-10 08:21:41 -0300

    implementado control props

--- src/02- components-patterns/hooks/use-product.ts
+++ src/02- components-patterns/hooks/use-product.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect,useState } from 'react'
 import { onChangeArgs, IProduct } from '../interfaces/interfaces';
 
 
@@ -13,13 +13,13 @@ export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => 
 
     const [ counter, setCounter ] = useState( value );
 
-    const isControlled = useRef( !!onChange )// con el useRef podemos saber si el onChange esta definido o no
+    // const isControlled = useRef( !!onChange )// con el useRef podemos saber si el onChange esta definido o no
 
     const increaseBy = ( value: number ) => {
 
-        if( isControlled.current ) {
-            return onChange!({ count: value, product });
-        }
+        // if( isControlled.current ) {
+        //     return onChange!({ count: value, product });
+        // }
         
         const newValue = Math.max( counter + value, 0 )
         setCounter( newValue );
